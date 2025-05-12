d61e66453f52ce62fa7ec6517ce8ffe183d3b662
Gael Arzubide <arzubide.ga@gmail.com>
2025-05-11 18:51:55 -0600

    Se agrega managers.py y se crea el modelo para trabajar

---
+++ Python/Django/GreenSaty/greenstay/applications/users/managers.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.contrib.auth.models import BaseUserManager
+
+class UserManager(BaseUserManager, models.Model):
+    
+    #Funcion base para la creacion de usuarios
+    def _create_user(self, nombres, apellidos, correo, contrasenia, is_staff, is_superuser, **extra_fields):
+        
+        usuario = self.model(
+            nombres = nombres,
+            apellidos = apellidos,
+            correo = correo,
+            is_staff = is_staff,
+            is_superuser = is_superuser,
+            **extra_fields
+        )
+
+        print(f"Contraseña ingresada: {contrasenia}")
+        usuario.set_password(contrasenia) #hasheamos
+        usuario.save(using=self.db) #Guardamos la contraseña en la base de datos
+        
+        return usuario
+    
+    #Funcion para la creacion de los usuarios
+    def create_user(self, nombres, apellidos, correo, contrasenia = None, **extra_fields):
+        return self._create_user(nombres, apellidos, correo, contrasenia, False, False, **extra_fields)
+
+    
+    #Funcion para la creacion de los administradores de la pagina
+
+
+
+    #Funcion para la creacion de los super usuarios
+    def create_superuser(self, nombres, apellidos, correo, contrasenia = None, **extra_fields):
+        return self._create_user(nombres, apellidos, correo, contrasenia, True, True, **extra_fields)
--- Python/Django/GreenSaty/greenstay/applications/users/models.py
+++ Python/Django/GreenSaty/greenstay/applications/users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from .managers import UserManager
 
-# Create your models here.
+class Usuarios(AbstractBaseUser,PermissionsMixin):
+    
+    nombres = models.CharField(max_length=25) 
+    apellidos = models.CharField(max_length=25)
+    correo = models.EmailField(unique=True)
+
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'correo'
+    REQUIRED_FIELDS = ['nombres,apellidos']
+
+    objects = UserManager()
