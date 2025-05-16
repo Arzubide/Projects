72a2bbd605c2d55b956c8f700763bdfd49b50c9a
Arzubide <proyects.arzu@gmail.com>
2025-05-16 14:18:57 -0600

    Se crea el forms para el registro de los usuarios

--- Python/Django/GreenSaty/greenstay/applications/administrador/forms.py
+++ Python/Django/GreenSaty/greenstay/applications/administrador/forms.py
@@ -1,4 +1,38 @@
 from django import forms
+from .models import RegistroPersonal
 
-class ModeloRegistroPersonal(forms.Form):
-    pass
+
+class ModeloRegistroPersonal(forms.ModelForm):
+    
+    class Meta:
+        model = RegistroPersonal
+        fields =(
+            'area',
+            'nombres',
+            'apellidos',
+            'correo',
+            'gerenteArea',
+        )
+
+        widgets = {
+            'area': forms.Select(
+                
+            ),
+            'nombres' : forms.TextInput(
+                attrs={
+                    'placeholder' : 'Ingresa tu nombre(s)'
+                }
+            ),
+            'apellidos' : forms.TextInput(
+                attrs={
+                    'placeholder' : 'Ingresa tus apellidos'
+                }
+            ),
+            'correo' : forms.EmailInput(
+                attrs={
+                    'placeholder' : 'Ingresa tu correo'
+                }
+            ),
+            'gerenteArea' : forms.CheckboxInput()
+        }
+    
