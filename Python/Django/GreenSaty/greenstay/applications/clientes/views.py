f4bbfbf34ac38fdba5cc9b37f2061b27deadab05
Arzubide <proyects.arzu@gmail.com>
2025-06-03 19:49:35 -0600

    se termina con las funcionalidades de editar el perfil del usuario

--- Python/Django/GreenSaty/greenstay/applications/clientes/views.py
+++ Python/Django/GreenSaty/greenstay/applications/clientes/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
-#Modelos
 from .forms import Tarjeta
+#Modelos
 from .models import TarjetaCredito
 from applications.habitaciones.models import Habitacion
 from applications.users.models import Usuarios
+from django.views import View
 #Vistas
-from django.views.generic import CreateView, DetailView
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.shortcuts import redirect, get_object_or_404
 
 class RegistroTarjeta(CreateView):
     template_name = 'clientes/registroTarjeta.html'
@@ -34,3 +36,29 @@ class DetallesCuenta(DetailView):
         HabitacionesUsurio = Habitacion.objects.filter(usuarioHabitacion=Usuario) #Variable modelo = varible de la funcion
         context['habitacion'] = HabitacionesUsurio
         return context
+
+
+class EliminarTarjetaCredito(DeleteView):
+    model = TarjetaCredito
+    template_name = 'clientes/eliminarTarjeta.html'
+    success_url = reverse_lazy('urls_home:inicioUser')
+
+
+class EliminarReservacion(View):
+    def post(self, request, pk):
+        habitacion = get_object_or_404(Habitacion, pk=pk, estadoHabitacion='RESERVADA')
+        habitacion.estadoHabitacion = 'DISPONIBLE'
+        habitacion.usuarioHabitacion = None
+        habitacion.save()
+        return redirect('urls_home:inicioUser')
+
+
+class ActualizarDatos(UpdateView):
+    model = Usuarios
+    template_name = 'clientes/editarDatos.html'
+    fields = [
+        'nombres',
+        'apellidos',
+        'correo',
+    ]
+    success_url = reverse_lazy('urls_home:inicioUser')
