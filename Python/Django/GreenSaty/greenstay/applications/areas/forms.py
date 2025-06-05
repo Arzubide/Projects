e95b1a8a0c11b55826a04de5576e7a5aed90ba83
Gael Arzubide <arzubide.ga@gmail.com>
2025-06-05 11:37:49 -0600

    se agrega el disenio del registro de una nueva area|

--- Python/Django/GreenSaty/greenstay/applications/areas/forms.py
+++ Python/Django/GreenSaty/greenstay/applications/areas/forms.py
@@ -1,6 +1,32 @@
 from django import forms
+from .models import AreasHotel
 
 
-class Registro(forms.Form):
-    nombreArea = forms.CharField(max_length=50)
-    horarioFuncionamiento = forms.CharField(max_length=15)
+class Registro(forms.ModelForm):
+    # nombreArea = forms.CharField(max_length=50)
+    # horarioFuncionamiento = forms.CharField(max_length=15)
+
+    class Meta:
+        model = AreasHotel
+        fields = (
+            'nombreArea',
+            'horarioFuncionamiento',
+        )
+
+        
+        widgets = {
+            'nombreArea' : forms.TextInput(
+                attrs = {
+                    'placeholder' : 'Ingrese el nombre del area',
+                    'class': 'form-control',
+                    'id' : 'nombreArea'
+                }
+            ),
+            'horarioFuncionamiento' : forms.TextInput(
+                attrs = {
+                    'placeholder' : 'nnAM - nnPM',
+                    'class': 'form-control',
+                    'id' : 'nombreArea'
+                }
+            )
+        }
