fa1458408afa73344827c136bcae2121a32030c5
Gael Arzubide <arzubide.ga@gmail.com>
2025-06-12 11:41:57 -0600

    Control de sesiones de la aplicacion de areas

--- Python/Django/GreenSaty/greenstay/applications/areas/views.py
+++ Python/Django/GreenSaty/greenstay/applications/areas/views.py
@@ -3,8 +3,11 @@ from django.views.generic import FormView, ListView, DeleteView,UpdateView
 from .forms import Registro
 from django.urls import reverse_lazy
 from .models import AreasHotel
+#Control de sesiones
+from applications.home.mixins import VistaAdministrador
 
-class RegistroArea(FormView):
+
+class RegistroArea(VistaAdministrador,FormView):
     template_name = 'areas/Creacion_servicios.html'
     form_class = Registro
     success_url = reverse_lazy('urls_home:inicioAdmin')
@@ -19,7 +22,7 @@ class RegistroArea(FormView):
         return super(RegistroArea, self).form_valid(form)
 
 
-class ListaAreasRegistradas(ListView):
+class ListaAreasRegistradas(VistaAdministrador,ListView):
     model = AreasHotel
     template_name = 'areas/ListaAreasRegistradas.html'
     context_object_name = 'areas'
@@ -28,13 +31,13 @@ class ListaAreasRegistradas(ListView):
         return AreasHotel.objects.all()
 
 
-class EliminarArea(DeleteView):
+class EliminarArea(VistaAdministrador,DeleteView):
     model = AreasHotel
     template_name = 'acciones/EliminarArea.html'
     success_url = reverse_lazy('urls_areas:ListaAreasRegistradas')
 
 
-class ModificarArea(UpdateView):
+class ModificarArea(VistaAdministrador,UpdateView):
     model = AreasHotel
     template_name = 'acciones/ModificarArea.html'
     form_class = Registro
