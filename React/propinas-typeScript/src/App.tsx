df3c116d2a0debbbc0348b21ffd2fe9cab3cb0d5
Arzubide <proyects.arzu@gmail.com>
2025-07-28 14:53:39 -0600

    se crea el componente de los items del menu y se muestra sin propiedades en app

--- React/propinas-typeScript/src/App.tsx
+++ React/propinas-typeScript/src/App.tsx
@@ -1,13 +1,30 @@
-
+import Item from "./components/Item"
+import { menuItems } from "./data/db" //Datos de la base de datos
 
 function App() {
   
-
   return (
     <>
       <header className="bg-teal-400 py-5">
         <h1 className="text-center text-4xl font-black">Calculadora de propinas y consumo</h1>
       </header>
+
+      <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
+        <div>
+          <h2>Menu</h2>
+            {menuItems.map(item => (
+              <Item
+               key={item.id} // Cada item debe tener un ID unico
+               
+              />
+            ))}
+        </div>
+
+        <div>
+          <h2>Consumo</h2>
+
+        </div>
+      </main>
     </>
   )
 }
---
+++ React/propinas-typeScript/src/components/Item.tsx
@@ -0,0 +1,8 @@
+// Solo agregando RFC(react function component), con la extension instalada ya te crea el componente automaticamente, solo eliminamos la primera linea de codigo que te genera
+
+export default function Item() {
+  return (
+    <div>Item</div>
+  )
+}
+
