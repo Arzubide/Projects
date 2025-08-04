fdf0f26feb663ff961f1da7dfc681cca8d84a636
Gael Arzubide <arzubide.ga@gmail.com>
2025-08-04 15:14:19 -0600

    Se crea el state para cada uno de los inputs y se setea correctamente

--- React/calorie-tracker/src/components/Form.tsx
+++ React/calorie-tracker/src/components/Form.tsx
@@ -3,11 +3,20 @@ import { useState } from "react"
 
 export default function Form() {
 
-    const [activiy, setActivity] = useState({
+    const [activity, setActivity] = useState({
         category : 1,
         name : '',
         calories : 0
     })
+
+
+    const handleChange = (e) => {
+        /* Con e.taget.id sabes que elemento es que esta seleccionando o de donde proviene la entrada. Con e.target.value obtenemos el valor ingresado por el usuario */
+        setActivity({
+            ...activity, // Hacemos una copia de lo que esta previamene seteado
+            [e.target.id] : e.target.value // Seteamos los nuevos valores  [KEY] : VALUE. Key va entre llaves para que ts lo interprete como variable y no como la llave literal (e.target.id)
+        })
+    }
 
   return (
     <form className="space-y-5 bg-white p-10 shadow rounded-2xl">
@@ -16,7 +25,8 @@ export default function Form() {
             <select 
                 className="border p-2 rounded-2xl w-full bg-white" 
                 id="category" 
-                value={activiy.category}>
+                value={activity.category}
+                onChange={handleChange}>
                 
                 {categories.map(category => (
                     <option
@@ -36,7 +46,8 @@ export default function Form() {
                 id="name" 
                 className="border p-2 rounded-2xl w-full bg-white" 
                 placeholder="Ej. Comida, Jugo de naranja, Ejercicio, Ensalada, Pesas, Bicicleta"
-                value={activiy.name}/>
+                value={activity.name}
+                onChange={handleChange}/>
         </div>
 
         <div className="grid grid-cols-1 gap-3">
@@ -46,7 +57,8 @@ export default function Form() {
                 id="calories" 
                 className="border p-2 rounded-2xl w-full bg-white" 
                 placeholder="Ej. 400. 500, etc"
-                value={activiy.calories}/>
+                value={activity.calories}
+                onChange={handleChange}/>
         </div>
 
         <input type="submit" className="bg-gray-600 text-white cursor-pointer w-full  p-2 hover:bg-gray-900 font-bold uppercase" value={'Guardar'}/>
