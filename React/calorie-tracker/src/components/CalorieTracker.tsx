da6bf3b833a25a98c40baca85ae2268cf5c43688
Gael Arzubide <arzubide.ga@gmail.com>
2025-08-12 15:17:48 -0600

    se crea el componente CaloriesTracker

---
+++ React/calorie-tracker/src/components/CalorieTracker.tsx
@@ -0,0 +1,13 @@
+import type { Activity } from "../types"
+
+type CalorieProps = {
+    activities : Activity[]
+}
+
+export default function CalorieTracker({activities} : CalorieProps) {
+  return (
+    <>
+        
+    </>
+  )
+}
