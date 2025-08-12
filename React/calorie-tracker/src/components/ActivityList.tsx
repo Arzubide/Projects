f7bad42643670c3558917c68a4b067a463a67dd5
Gael Arzubide <arzubide.ga@gmail.com>
2025-08-12 13:53:31 -0600

    se agrega texto cuando no hay actividades registradas

--- React/calorie-tracker/src/components/ActivityList.tsx
+++ React/calorie-tracker/src/components/ActivityList.tsx
@@ -20,43 +20,50 @@ export default function ActivityList({state, dispatch} : ActivityProps ) {
             Comida y actividades
         </h2>
 
-        {state.map( activity => (
-            <div key={activity.id} className="px-5 py-10 bg-gray-500 mt-5 flex justify-between">
-                {/* Div izquierda, contenido de lo registrado */}
-                <div className="space-y-2 relative">
-                    <p className={`absolute -top-8 -left-8 px-10 py-2 text-white uppercase font-bold 
-                        ${activity.category === 1 ? 'bg-lime-500' : 'bg-orange-500'}`}>
-                        {categoryName(+activity.category)}
-                    </p> {/* Recibimos 1 u dos */}
-                    <p className="text-2xl font-bold pt-5">
-                        {activity.name}
-                    </p>
-                    <p className="font-black text-4xl text-lime-500">
-                        {activity.calories} 
-                        <span> Calorias</span>
-                    </p>
-                </div>
-                {/* Div derecha, modificacion o eliminar contenido */}
-                <div className="flex gap-5 items-center">
-                    <button
-                    onClick={()=>dispatch({type : "setActiveId", payload : {id : activity.id }})}
-                    >
-                        <PencilSquareIcon // Importamos el icono 
-                            className="h-8 w-8 text-gray-800 hover:text-white"
-                        />
-                    </button>
-                    <button
-                        className="flex gap-5 items-center"
-                        onClick={() => dispatch({type : 'delete-Activity' , payload : {id:activity.id}})}
-                    >
-                        <XCircleIcon
-                            className="h-8 w-8 text-gray-800 hover:text-white"
-                        />
-                    </button>
+        {state.length === 0 ? 
+            <p
+            className="text-center py-3 font-bold"
+            >
+                No hay actividades registradas</p> 
+        :
+            state.map( activity => (
+                <div key={activity.id} className="px-5 py-10 bg-gray-500 mt-5 flex justify-between">
+                    {/* Div izquierda, contenido de lo registrado */}
+                    <div className="space-y-2 relative">
+                        <p className={`absolute -top-8 -left-8 px-10 py-2 text-white uppercase font-bold 
+                            ${activity.category === 1 ? 'bg-lime-500' : 'bg-orange-500'}`}>
+                            {categoryName(+activity.category)}
+                        </p> {/* Recibimos 1 u dos */}
+                        <p className="text-2xl font-bold pt-5">
+                            {activity.name}
+                        </p>
+                        <p className="font-black text-4xl text-lime-500">
+                            {activity.calories} 
+                            <span> Calorias</span>
+                        </p>
+                    </div>
+                    {/* Div derecha, modificacion o eliminar contenido */}
+                    <div className="flex gap-5 items-center">
+                        <button
+                        onClick={()=>dispatch({type : "setActiveId", payload : {id : activity.id }})}
+                        >
+                            <PencilSquareIcon // Importamos el icono 
+                                className="h-8 w-8 text-gray-800 hover:text-white"
+                            />
+                        </button>
+                        <button
+                            className="flex gap-5 items-center"
+                            onClick={() => dispatch({type : 'delete-Activity' , payload : {id:activity.id}})}
+                        >
+                            <XCircleIcon
+                                className="h-8 w-8 text-gray-800 hover:text-white"
+                            />
+                        </button>
 
+                    </div>
                 </div>
-            </div>
-        ))}
+            ))
+        }
     </>
  )
 }
