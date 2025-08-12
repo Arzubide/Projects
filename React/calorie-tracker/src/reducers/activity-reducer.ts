fee6896d6e86b8f6345a7a9b93748d4042b1564b
Gael Arzubide <arzubide.ga@gmail.com>
2025-08-12 12:29:31 -0600

    Se modifica el reducer para darle el funcionamiento al boton de editar

--- React/calorie-tracker/src/reducers/activity-reducer.ts
+++ React/calorie-tracker/src/reducers/activity-reducer.ts
@@ -5,19 +5,20 @@ export type ActivityActions = {
     /* En un reducer, las acciones (action) siempre tienen:
         type → indica qué acción quieres realizar */
     type : 'save-activity', // El type describe que es lo que esta susediendo
-    payload : { newActivity : Activity }// Payload son los datos que se van agregar o que vamos almacenar
+    payload : { newActivity : Activity }// Payload son los datos que se van agregar o que vamos almacenar 
+} | {type : 'setActiveId', payload: {id: Activity['id']}} // Agregamos una nueva accions a nuestro reducer, en este caso solo recolectamos el id de la actividad para identificarla y poder editar
 
-}
-
-type ActivityState ={
-    activities : Activity[]
+type ActivityState = {
+    activities : Activity[],
+    activeID : string
 }
 
 export const initialState : ActivityState = {
     /* Este es el estado inicial del reducer.
     Aquí defines con qué datos empieza tu aplicación antes de que pase nada.
     En este caso, comienza con activities como un arreglo vacío. */
-    activities : []
+    activities : [],
+    activeID : ''
 }
 
 export const activityReducer = (
@@ -33,6 +34,13 @@ export const activityReducer = (
         return { // retorna el estado actualizado
             ...state,
             activities : [...state.activities, action.payload.newActivity] // Hacemos copia de las actividades previas y concatenamos con la nueva informacion ingresada
+        }
+    }
+
+    if (action.type === 'setActiveId'){
+        return {
+            ...state, // Para mantener los registros anteriores
+            activeId : action.payload.id // Obtenemos el id de la actividad
         }
     }
     return state // La logica del reducer siempre debe tener un return
