83518887ee3462591b31c57c353a6a2112414e8b
Gael Arzubide <arzubide.ga@gmail.com>
2025-08-12 15:44:13 -0600

    se agrega el apartado de calorias consumidas y quemadas en pantalla

--- React/calorie-tracker/src/components/CalorieTracker.tsx
+++ React/calorie-tracker/src/components/CalorieTracker.tsx
@@ -1,13 +1,34 @@
+import { useMemo } from "react"
 import type { Activity } from "../types"
+import CalorieDisplay from "./CalorieDisplay"
 
 type CalorieProps = {
     activities : Activity[]
 }
 
 export default function CalorieTracker({activities} : CalorieProps) {
-  return (
-    <>
-        
-    </>
-  )
+
+    //Contadores
+    const caloriesCosumed = useMemo(()=> activities.reduce((total, activity) => activity.category === 1 ? total + activity.calories : total,0),[activities])
+
+    const caloriesBurned = useMemo(()=> activities.reduce((total, activity) => activity.category === 2 ? total + activity.calories : total,0),[activities])
+
+    return (
+        <>
+            <h2 className="text-4xl font-black text-white text-center">Resumen de calorias</h2>
+
+            
+
+            <div className="flex flex-col items-center md:flex-row md:justify-between gap-5 mt-10">
+                <CalorieDisplay
+                    calories={caloriesCosumed}
+                    text="Consumidas"
+                />
+                <CalorieDisplay
+                    calories={caloriesBurned}
+                    text="Quemadas"
+                />
+            </div>
+        </>
+    )
 }
