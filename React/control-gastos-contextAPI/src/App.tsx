be1cf2d8ec166cd2e306c99bb063082311319c8c
Arzubide <proyects.arzu@gmail.com>
2025-08-15 12:19:49 -0600

    se crea un hook perzonalizado para utilizar state y dispatch

--- React/control-gastos-contextAPI/src/App.tsx
+++ React/control-gastos-contextAPI/src/App.tsx
@@ -2,19 +2,21 @@ import BudgetForm from "./components/BudgetForm"
 
 function App() {
 
-  return (
-    <>
-      <header className="bg-blue-600 py-8 max-h-72">
-        <h1 className="uppercase text-center font-black text-4xl text-white">Planificador de gastos</h1>
-      </header>
+    // const context = useContext(BudgetContext) Creamos esta variable utilizando el hook useContext para acceder a nuestro estado con una sola variable pero existe otra alternativa que es crear un custom hook useBudget.
 
-      <div className="max-w-3xl mx-auto shadow-2xl rounded-lg mt-10 p-10 bg-white">
-        <BudgetForm
-        
-        />
-      </div>
-    </>
-  )
+    return (
+      <>
+        <header className="bg-blue-600 py-8 max-h-72">
+          <h1 className="uppercase text-center font-black text-4xl text-white">Planificador de gastos</h1>
+        </header>
+
+        <div className="max-w-3xl mx-auto shadow-2xl rounded-lg mt-10 p-10 bg-white">
+          <BudgetForm
+          
+          />
+        </div>
+      </>
+    )
 }
 
 export default App
---
+++ React/control-gastos-contextAPI/src/hooks/useBudget.ts
@@ -0,0 +1,7 @@
+import { useContext } from "react";
+import { BudgetContext } from "../context/BudgetContext";
+
+export const useBudet = () => {
+    const context = useContext(BudgetContext) // Se crea una variable context usando el hook useContext pasandole la variable BudgetContext que contiene state y dispatch de useReducer
+    return context
+}
