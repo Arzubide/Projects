08fca86798ed747998a66144f047a986f38a8523
Arzubide <proyects.arzu@gmail.com>
2025-08-19 17:05:04 -0600

    se agrega swiped al los componentes pero no funcionan aun

--- React/control-gastos-contextAPI/src/components/ExpenseDetail.tsx
+++ React/control-gastos-contextAPI/src/components/ExpenseDetail.tsx
@@ -1,3 +1,13 @@
+import {
+  LeadingActions,
+  SwipeableList,
+  SwipeableListItem,
+  SwipeAction,
+  TrailingActions,
+} from 'react-swipeable-list';
+import 'react-swipeable-list/dist/styles.css';
+
+
 import { useMemo } from "react";
 import { formatDate } from "../helpers";
 import type { Expense } from "../types";
@@ -11,24 +21,52 @@ export default function ExpenseDetail({expense} : ExpenseDetailProps) {
     const categoryInfo = useMemo(() => categories.filter(cat => cat.id === expense.category)[0] ,[expense]) // expense.category almacena un string del 1-7
     // agregamos el [0] para traer el contenido de la posicion en lugar de devolverlos todo el arreglo
 
+
+    // Funciones de swipeable-list
+    const leadingActions = () => ( // Funcion que define el comportamiento de izqiuierda a derecha
+        <LeadingActions>
+            <SwipeAction onClick={() => {}}>
+                Actualizar
+            </SwipeAction>
+        </LeadingActions>
+        );
+
+    const trailingActions = () => ( // Funcion que define el comportamiento de derecha a izquierda
+        <TrailingActions>
+            <SwipeAction
+            // destructive={true}
+            onClick={() => {}}
+            >
+                Eliminar
+            </SwipeAction>
+        </TrailingActions>
+    );
+
     return (
-            <div className=" bg-white shadow-2xl p-10 w-fullb border-b border-gray-200 flex gap-5">
-                <div>
-                    <img src={`/icono_${categoryInfo.icon}.svg`} alt="" className="w-20 space-y-3" />
-                </div>
+        <SwipeableList>
+            <SwipeableListItem
+                leadingActions={leadingActions()}
+                trailingActions={trailingActions()}
+            >
+                <div className="shadow-2xl  border-b bg-white border-gray-200 p-10 flex gap-5 w-full">
+                    <div>
+                        <img src={`/icono_${categoryInfo.icon}.svg`} alt="" className="w-20 space-y-3" />
+                    </div>
 
-                <div className="flex-10"> {/* flex1 toma todo el ancho disponible */}
-                    <p className="text-sm font-bold uppercase text-slate-500">{categoryInfo.name}</p>
-                    <p>{expense.expenseName}</p>
-                    <p className="text-slate-600 text-sm">{formatDate(expense.date!.toString())}</p>
-                </div>
+                    <div className="flex-10"> {/* flex1 toma todo el ancho disponible */}
+                        <p className="text-sm font-bold uppercase text-slate-500">{categoryInfo.name}</p>
+                        <p>{expense.expenseName}</p>
+                        <p className="text-slate-600 text-sm">{formatDate(expense.date!.toString())}</p>
+                    </div>
 
-                <div className="py-5">
-                    <AmountDisplay
-                    amount={expense.amount}
-                />
+                    <div className="py-5">
+                        <AmountDisplay
+                        amount={expense.amount}
+                    />
+                    </div>
+                    
                 </div>
-                
-            </div>
+            </SwipeableListItem>
+        </SwipeableList>
     )
 }
