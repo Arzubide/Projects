14cca6db01cce8fd833c233d00a524ed283225f7
Arzubide <proyects.arzu@gmail.com>
2025-08-19 15:45:25 -0600

    se agregan iconos y se da diseño, junto con la funcion que muestra las iconos ids

--- React/control-gastos-contextAPI/src/components/ExpenseDetail.tsx
+++ React/control-gastos-contextAPI/src/components/ExpenseDetail.tsx
@@ -1,24 +1,34 @@
+import { useMemo } from "react";
 import { formatDate } from "../helpers";
 import type { Expense } from "../types";
 import AmountDisplay from "./AmountDisplay";
+import { categories } from "../data/categories";
 type ExpenseDetailProps = {
     expense : Expense
 }
 export default function ExpenseDetail({expense} : ExpenseDetailProps) {
+    
+    const categoryInfo = useMemo(() => categories.filter(cat => cat.id === expense.category)[0] ,[expense]) // expense.category almacena un string del 1-7
+    // agregamos el [0] para traer el contenido de la posicion en lugar de devolverlos todo el arreglo
+
     return (
             <div className=" bg-white shadow-2xl p-10 w-fullb border-b border-gray-200 flex gap-5">
                 <div>
-
+                    <img src={`/icono_${categoryInfo.icon}.svg`} alt="" className="w-20 space-y-3" />
                 </div>
 
-                <div>
+                <div className="flex-10"> {/* flex1 toma todo el ancho disponible */}
+                    <p className="text-sm font-bold uppercase text-slate-500">{categoryInfo.name}</p>
                     <p>{expense.expenseName}</p>
                     <p className="text-slate-600 text-sm">{formatDate(expense.date!.toString())}</p>
                 </div>
 
-                <AmountDisplay
+                <div className="py-5">
+                    <AmountDisplay
                     amount={expense.amount}
                 />
+                </div>
+                
             </div>
     )
 }
