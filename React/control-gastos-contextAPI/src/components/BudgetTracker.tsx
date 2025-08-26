7fce94ed14cd02262987b146ad622f58afe9c467
Arzubide <proyects.arzu@gmail.com>
2025-08-25 20:14:54 -0600

    se agrega barra interactiva con los saldos

--- React/control-gastos-contextAPI/src/components/BudgetTracker.tsx
+++ React/control-gastos-contextAPI/src/components/BudgetTracker.tsx
@@ -1,15 +1,25 @@
+import {CircularProgressbar, buildStyles} from 'react-circular-progressbar'
 import { useBudget } from "../hooks/useBudget"
 import AmountDisplay from "./AmountDisplay"
+import 'react-circular-progressbar/dist/styles.css'
 
 export default function BudgetTracker() {
 
     const {state, available, spent} = useBudget()
-
+    const bar = +((available*100)/state.budget)
     return (
         <>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
                 <div className="flex justify-center">
-                    <img src="/grafico.jpg" alt="Imagen con graficos" />
+                    <CircularProgressbar
+                        value={bar}
+                        styles={buildStyles({
+                            pathColor : bar < 30 ? '#dc2626' : '#3b82f6',
+                            trailColor : '#F5F5F5',
+                            textColor : '#3b82f6'
+                        })}
+                        text={`${bar}%`}
+                    />
                 </div>
                 <div className="flex flex-col justify-center items-center gap-8">
                     <AmountDisplay
