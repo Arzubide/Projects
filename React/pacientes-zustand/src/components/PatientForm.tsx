974af75a06d67248e570e13355e6b784bc69d7a5
Arzubide <proyects.arzu@gmail.com>
2025-09-01 18:04:44 -0600

    se agrega un componente para mostrar todos los errores para las validaciones agregadas

--- React/pacientes-zustand/src/components/PatientForm.tsx
+++ React/pacientes-zustand/src/components/PatientForm.tsx
@@ -1,82 +1,116 @@
+import { useForm } from 'react-hook-form'
+import Error from './Error'
+
 export default function PatientForm() {
   
-  return (
-    <div className="md:w-1/2 lg:w-2/5 mx-5">
-        <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
+    const {register, handleSubmit, formState : {errors}} = useForm() 
+    // register = This method allows you to register an input or select element and apply validation rules to React Hook Form
+    // handleSubmit = This function will receive the form data if form validation is successful.
+    // formState : {errors} = accedemos solamente a los errores y podemos mostrarlos
 
-        <p className="text-lg mt-5 text-center mb-10">
-            Añade Pacientes y {''}
-            <span className="text-indigo-600 font-bold">Administralos</span>
-        </p>
 
-        <form 
-            className="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
-            noValidate
-        >
-              <div className="mb-5">
-                  <label htmlFor="name" className="text-sm uppercase font-bold">
-                      Paciente 
-                  </label>
-                  <input  
-                      id="name"
-                      className="w-full p-3  border border-gray-100"  
-                      type="text" 
-                      placeholder="Nombre del Paciente" 
-                  />
-              </div>
+    const registerPatient = () => {
+        // Si todas las validaciones pasan, se ejecuta esta funcion
+        console.log('data, e')
+    }
 
-              <div className="mb-5">
-                <label htmlFor="caretaker" className="text-sm uppercase font-bold">
-                    Propietario 
+    return (
+        <div className="md:w-1/2 lg:w-2/5 mx-5">
+            <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
+
+            <p className="text-lg mt-5 text-center mb-10">
+                Añade Pacientes y {''}
+                <span className="text-indigo-600 font-bold">Administralos</span>
+            </p>
+
+            <form 
+                className="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
+                noValidate
+                onSubmit={handleSubmit(registerPatient)}
+            >
+                <div className="mb-5">
+                    <label htmlFor="name" className="text-sm uppercase font-bold">
+                        Paciente 
+                    </label>
+                    <input  
+                        id="name"
+                        className="w-full p-3  border border-gray-100"  
+                        type="text" 
+                        placeholder="Nombre del Paciente"
+                        {...register('name', { // Forma de agregar useForm a un campo de nuestro formulario
+                            // Dentro de los corchetes va los requerimientos que se solicita
+                            required : 'error message',
+                            minLength : {
+                                value : 8,
+                                message : 'Minimo 8 caracteres'
+                            }
+                        })}
+                    />
+                    {errors.name && ( // Mostramos el error unicamente cuando exista un error
+                        <Error>
+                            {errors.name?.message as String} {/* Se agrega as string para remover el error que sale por TypeScrpt */}
+                        </Error>
+                    )}
+
+                    {errors.min && ( // Mostramos el error unicamente cuando exista un error
+                        <Error>
+                            {errors.name?.message as String} {/* Se agrega as string para remover el error que sale por TypeScrpt */}
+                        </Error>
+                    )}
+                </div>
+
+                <div className="mb-5">
+                    <label htmlFor="caretaker" className="text-sm uppercase font-bold">
+                        Propietario 
+                    </label>
+                    <input  
+                        id="caretaker"
+                        className="w-full p-3  border border-gray-100"  
+                        type="text" 
+                        placeholder="Nombre del Propietario" 
+                    />
+                </div>
+
+                <div className="mb-5">
+                <label htmlFor="email" className="text-sm uppercase font-bold">
+                    Email 
                 </label>
                 <input  
-                    id="caretaker"
+                    id="email"
                     className="w-full p-3  border border-gray-100"  
-                    type="text" 
-                    placeholder="Nombre del Propietario" 
+                    type="email" 
+                    placeholder="Email de Registro" 
                 />
-              </div>
+                </div>
 
-            <div className="mb-5">
-              <label htmlFor="email" className="text-sm uppercase font-bold">
-                  Email 
-              </label>
-              <input  
-                  id="email"
-                  className="w-full p-3  border border-gray-100"  
-                  type="email" 
-                  placeholder="Email de Registro" 
-              />
-            </div>
+                <div className="mb-5">
+                    <label htmlFor="date" className="text-sm uppercase font-bold">
+                        Fecha Alta 
+                    </label>
+                    <input  
+                        id="date"
+                        className="w-full p-3  border border-gray-100"  
+                        type="date" 
+                    />
+                </div>
+                
+                <div className="mb-5">
+                    <label htmlFor="symptoms" className="text-sm uppercase font-bold">
+                    Síntomas 
+                    </label>
+                    <textarea  
+                        id="symptoms"
+                        className="w-full p-3  border border-gray-100"  
+                        placeholder="Síntomas del paciente" 
+                    ></textarea>
+                </div>
 
-            <div className="mb-5">
-                <label htmlFor="date" className="text-sm uppercase font-bold">
-                    Fecha Alta 
-                </label>
-                <input  
-                    id="date"
-                    className="w-full p-3  border border-gray-100"  
-                    type="date" 
+                <input
+                    type="submit"
+                    className="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-colors"
+                    value='Guardar Paciente'
                 />
-            </div>
-            
-            <div className="mb-5">
-                <label htmlFor="symptoms" className="text-sm uppercase font-bold">
-                Síntomas 
-                </label>
-                <textarea  
-                    id="symptoms"
-                    className="w-full p-3  border border-gray-100"  
-                    placeholder="Síntomas del paciente" 
-                ></textarea>
-            </div>
-
-            <input
-                type="submit"
-                className="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-colors"
-                value='Guardar Paciente'
-            />
-        </form> 
-    </div>
-  )
+            </form> 
+        </div>
+    )
 }
