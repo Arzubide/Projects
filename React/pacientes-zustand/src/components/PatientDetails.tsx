e2600957be355e3a0490751fba1926765d73e916
Arzubide <proyects.arzu@gmail.com>
2025-09-11 15:00:21 -0600

    se crea un nuevo componente para mostrar los datos los pacientes registrados

--- React/pacientes-zustand/src/components/PatientDetails.tsx
+++ React/pacientes-zustand/src/components/PatientDetails.tsx
@@ -1,4 +1,5 @@
 import type { PatientData } from "../types"
+import PatientDetailItem from "./PatientDetailItem"
 
 type PatientDetailsProps = {
     paciente : PatientData
@@ -6,8 +7,36 @@ type PatientDetailsProps = {
 
 export default function PatientDetails({paciente} : PatientDetailsProps) {
   return (
-        <div className="">
-            <p><span></span></p>
+        <div className="mx-5 my-5 px-5 py-5 bg-white shadow-2xl rounded-2xl">
+            <PatientDetailItem
+                label={'ID'}
+                data={paciente.id}
+            />
+
+            <PatientDetailItem
+                label={'Nombre'}
+                data={paciente.name}
+            />
+
+            <PatientDetailItem
+                label={'Propietario'}
+                data={paciente.caretaker}
+            />
+            
+            <PatientDetailItem
+                label={'Email'}
+                data={paciente.email}
+            />
+
+            <PatientDetailItem
+                label={'Fecha alta'}
+                data={paciente.date.toString()}
+            />
+
+            <PatientDetailItem
+                label={'Sintomas'}
+                data={paciente.symptoms}
+            />
         </div>
   )
 }
