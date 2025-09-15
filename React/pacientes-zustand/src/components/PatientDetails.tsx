58bebc3c3df23eff82d782a7d557aadb9ef8a0a1
Gael Arzubide <arzubide.ga@gmail.com>
2025-09-15 11:50:34 -0600

    se agrega la funcion de eliminar al boton eliminar

--- React/pacientes-zustand/src/components/PatientDetails.tsx
+++ React/pacientes-zustand/src/components/PatientDetails.tsx
@@ -1,58 +1,63 @@
 import type { PatientData } from "../types"
 import PatientDetailItem from "./PatientDetailItem"
+import { usePatientStore } from "../store/store"
 
 type PatientDetailsProps = {
     paciente : PatientData
 }
 
 export default function PatientDetails({paciente} : PatientDetailsProps) {
-  return (
-        <div className="mx-5 my-5 px-5 py-5 bg-white shadow-2xl rounded-2xl">
-            <PatientDetailItem
-                label={'ID'}
-                data={paciente.id}
-            />
 
-            <PatientDetailItem
-                label={'Nombre'}
-                data={paciente.name}
-            />
+   const {deletePatient} = usePatientStore() // Llamos la funcion para eliminar a los pacientes desde zustand
 
-            <PatientDetailItem
-                label={'Propietario'}
-                data={paciente.caretaker}
-            />
-            
-            <PatientDetailItem
-                label={'Email'}
-                data={paciente.email}
-            />
+    return (
+            <div className="mx-5 my-5 px-5 py-5 bg-white shadow-2xl rounded-2xl">
+                <PatientDetailItem
+                    label={'ID'}
+                    data={paciente.id}
+                />
 
-            <PatientDetailItem
-                label={'Fecha alta'}
-                data={paciente.date.toString()}
-            />
+                <PatientDetailItem
+                    label={'Nombre'}
+                    data={paciente.name}
+                />
 
-            <PatientDetailItem
-                label={'Sintomas'}
-                data={paciente.symptoms}
-            />
+                <PatientDetailItem
+                    label={'Propietario'}
+                    data={paciente.caretaker}
+                />
+                
+                <PatientDetailItem
+                    label={'Email'}
+                    data={paciente.email}
+                />
 
-            <div className="flex justify-between gap-3 mt-10">
-                <button
-                    type="button"
-                    className="py-2 px-10 bg-indigo-600 hover:bg-indigo-800 text-white font-bold uppercase rounded-2xl"
-                >
-                    Editar
-                </button>
+                <PatientDetailItem
+                    label={'Fecha alta'}
+                    data={paciente.date.toString()}
+                />
 
-                <button
-                    type="button"
-                    className="py-2 px-10 bg-red-600 hover:bg-red-800 text-white font-bold uppercase rounded-2xl"
-                >
-                    Eliminar
-                </button>
+                <PatientDetailItem
+                    label={'Sintomas'}
+                    data={paciente.symptoms}
+                />
+
+                <div className="flex justify-between gap-3 mt-10">
+                    <button
+                        type="button"
+                        className="py-2 px-10 bg-indigo-600 hover:bg-indigo-800 text-white font-bold uppercase rounded-2xl"
+                    >
+                        Editar
+                    </button>
+
+                    <button
+                        type="button"
+                        className="py-2 px-10 bg-red-600 hover:bg-red-800 text-white font-bold uppercase rounded-2xl"
+                        onClick={() => deletePatient(paciente.id)} // le pasamos la fucnion de eliminar al boton
+                    >
+                        Eliminar
+                    </button>
+                </div>
             </div>
-        </div>
-  )
+    )
 }
