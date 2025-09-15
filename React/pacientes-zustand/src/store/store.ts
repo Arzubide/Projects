4cf3763d7ecbc57bdb1f35732f8e134c25960c62
Gael Arzubide <arzubide.ga@gmail.com>
2025-09-15 12:03:39 -0600

    creamos getId y se crea la funcion para setear

--- React/pacientes-zustand/src/store/store.ts
+++ React/pacientes-zustand/src/store/store.ts
@@ -6,22 +6,23 @@ import {v4 as uidv4} from 'uuid'
 
 type PatientSate ={
     patients : PatientData[]
+    activeId : PatientData['id']
     addPatient : (data : draftPatientData) => void
     deletePatient : (id : PatientData['id']) => void
+    getPatientbyId : (id : PatientData['id']) => void
 }
 
 const createPatient = (patient : draftPatientData) : PatientData => { // Recibe un paciente de tipo draft y retorna un paciente 
     return { // Retornamos un paciente pero ahora con un id
         ...patient, id: uidv4()
     }
-
-
 }
 
 export const usePatientStore = create<PatientSate>((set)=>({ // SET permite actualizar el estado global
     // usePatientStore crea un estado global con la lista de pacientes (patients).
     // Cualquier componente puede leer o modificar ese estado usando el hook usePatientStore.
     patients : [], // Valor incial del estado global
+    activeId : '',
     addPatient : (data)=>{
         const newPatien = createPatient(data) // le pasamos el paciente a la funcion y guradamos el valor en newPatient
         set((state)=>({ // STATE representa el estado actual, en este caso accedemos al estado de patients que es nuestro valor iniciado
@@ -32,6 +33,11 @@ export const usePatientStore = create<PatientSate>((set)=>({ // SET permite actu
         set((state) => ({
             patients : state.patients.filter( patient => patient.id !== id) // Accedemos la lista de pacientes y retornamos los pacientes que sean diferentes del ID a eliminar
         }))
-    }
+    },
+    getPatientbyId: (id) => {
+        set(()=>({
+            activeId : id // Seteamos el id que obtivimos en el estado global
+        }))
+    },
 
 })) 
