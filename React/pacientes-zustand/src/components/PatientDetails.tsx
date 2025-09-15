bfcd18e26ee8e5de972abdd6af50e34680446f2d
Gael Arzubide <arzubide.ga@gmail.com>
2025-09-15 11:39:37 -0600

    se agregan los botones de eliminar y editar sin funcionalidad

--- React/pacientes-zustand/src/components/PatientDetails.tsx
+++ React/pacientes-zustand/src/components/PatientDetails.tsx
@@ -37,6 +37,22 @@ export default function PatientDetails({paciente} : PatientDetailsProps) {
                 label={'Sintomas'}
                 data={paciente.symptoms}
             />
+
+            <div className="flex justify-between gap-3 mt-10">
+                <button
+                    type="button"
+                    className="py-2 px-10 bg-indigo-600 hover:bg-indigo-800 text-white font-bold uppercase rounded-2xl"
+                >
+                    Editar
+                </button>
+
+                <button
+                    type="button"
+                    className="py-2 px-10 bg-red-600 hover:bg-red-800 text-white font-bold uppercase rounded-2xl"
+                >
+                    Eliminar
+                </button>
+            </div>
         </div>
   )
 }
