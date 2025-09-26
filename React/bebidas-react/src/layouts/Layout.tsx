a55b2763e321e2a738ba078e0d59833f13803013
Gael Arzubide <arzubide.ga@gmail.com>
2025-09-25 19:56:48 -0600

    creamos layout para el header

---
+++ React/bebidas-react/src/layouts/Layout.tsx
@@ -0,0 +1,8 @@
+import Header from "../components/Header";
+
+// Layout creado para mostrar componentes que tengan en comun paginas
+export default function Layout() {
+    return (
+        <Header/>
+    )
+}
