8ecfe54ed881822fb3ac80863377fe78c1423e68
Gael Arzubide <arzubide.ga@gmail.com>
2025-10-16 17:22:55 -0600

    se agrega el type de las categorias

---
+++ React/bebidas-react/src/types/index.ts
@@ -0,0 +1,4 @@
+import {z} from 'zod'
+import { CategoriesAPIREsponseSchema } from '../utils/recipes-schema'
+
+export type Categories = z.infer<typeof CategoriesAPIREsponseSchema> // Definimos el type de categorias basandonos en el esquema creado que recibe los datos de la API
