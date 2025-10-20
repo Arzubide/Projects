87e28ef83cd94d202ad1e503bf7e443c51925b37
Gael Arzubide <arzubide.ga@gmail.com>
2025-10-20 17:34:03 -0600

    se agrega el type del buscador

--- React/bebidas-react/src/types/index.ts
+++ React/bebidas-react/src/types/index.ts
@@ -1,4 +1,5 @@
 import {z} from 'zod'
-import { CategoriesAPIREsponseSchema } from '../utils/recipes-schema'
+import { CategoriesAPIREsponseSchema, searchFilterSchema } from '../utils/recipes-schema'
 
 export type Categories = z.infer<typeof CategoriesAPIREsponseSchema> // Definimos el type de categorias basandonos en el esquema creado que recibe los datos de la API
+export type SearchFilter = z.infer<typeof searchFilterSchema>
