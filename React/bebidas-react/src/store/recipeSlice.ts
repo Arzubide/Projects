95d890be2a73f1a41825cc65d729af776510c727
Gael Arzubide <arzubide.ga@gmail.com>
2025-10-20 17:48:10 -0600

    se guardan los filtros en searchRecipies

--- React/bebidas-react/src/store/recipeSlice.ts
+++ React/bebidas-react/src/store/recipeSlice.ts
@@ -1,10 +1,11 @@
 import type { StateCreator } from "zustand"
 import { getCategories } from "../Services/RecipeService"
-import type { Categories } from "../types"
+import type { Categories, SearchFilter } from "../types"
 
 export type RecipesSliceType ={
     categories : Categories
     fetchCategories: () => Promise<void>
+    searchRecipes: (searchFilter : SearchFilter) => Promise<void>
 }
 
 export const createRecipeSlice : StateCreator<RecipesSliceType> = (set) => (
@@ -17,6 +18,9 @@ export const createRecipeSlice : StateCreator<RecipesSliceType> = (set) => (
             set({
                 categories
             })
+        },
+        searchRecipes : async (filter) => { // Con searchFilter, obtenemos los datos ingresados en el formulario de header, los datos los tiene filter, tanto categoria como el input
+            console.log(filter)
         }
     }
 )
