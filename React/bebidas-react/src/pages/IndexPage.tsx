f8f17dd9c89960cdc4861546dd935dc07cef7006
Gael Arzubide <arzubide.ga@gmail.com>
2025-10-22 18:01:00 -0600

    rederizamos las bebidas que estan en el state de drinks

--- React/bebidas-react/src/pages/IndexPage.tsx
+++ React/bebidas-react/src/pages/IndexPage.tsx
@@ -1,10 +1,30 @@
+import { useMemo } from "react"
+import { useAppStore } from "../store/useAppStore"
+import DrinkCard from "../components/DrinkCard"
 
 export default function IndexPage() {
-     
+    
+    const drinks = useAppStore((state)=>state.drinks) // Obtenemos los datos que se almacenan en drinks desde el store
+    const hasDrinks = useMemo(()=> drinks.drinks.length >0 ,[drinks]) // si tenemos drinks en el store
+
     return (
         <>
-            <div>
-                <h1>Inicio</h1>
+            <div className="border-2 border-gray-200 p-5 rounded-2xl">
+                <h1 className="text-2xl text-center font-extrabold">Bebidas</h1>
+                {hasDrinks ? (
+                    <div>
+                        {drinks.drinks.map((drink) => (
+                            <DrinkCard
+                                key={drink.idDrink}
+                                drink={drink}
+                            />
+                        ))}
+                    </div>
+                ): (
+                    <div>
+                        <p className="text-center mt-5 mb-5 text-gray-500">Busca una bebida</p>
+                    </div>
+                )}
             </div>
         </>
     )
