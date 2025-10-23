a402d5d065d2d704791114d82b7663c7d0daa7b4
Gael Arzubide <arzubide.ga@gmail.com>
2025-10-22 18:18:24 -0600

    se da disenio a las cartas

--- React/bebidas-react/src/components/DrinkCard.tsx
+++ React/bebidas-react/src/components/DrinkCard.tsx
@@ -6,8 +6,23 @@ type DrinkCardProps = {
 
 export default function DrinkCard({drink} : DrinkCardProps) {
     return (
-        <div>
-            <h2>{drink.strDrink}</h2>
+        <div className="border border-gray-300 rounded-2xl p-3 shadow-2xl mb-15 ml-2.5 mr-2.5">
+            <div>
+                <img 
+                    className="rounded-2xl text-center"
+                    src={drink.strDrinkThumb}
+                    alt="Imagen de bebida" />
+            </div>
+
+            <div className="p-5">
+                <h2 className="text-2xl truncate font-black">{drink.strDrink}</h2>
+                <button
+                    type="button"
+                    className="bg-orange-400 hover:bg-orange-500 cursor-pointer p-5 rounded-2xl text-white w-full"
+                >
+                    Mas detalles
+                </button>
+            </div>
         </div>
     )
 }
