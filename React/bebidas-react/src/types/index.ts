8e457b203e958ef9e352fdf1cdeecb6f7fa01032
Gael Arzubide <arzubide.ga@gmail.com>
2025-10-22 18:01:57 -0600

    Creamos types para bebidas y bebida

--- React/bebidas-react/src/types/index.ts
+++ React/bebidas-react/src/types/index.ts
@@ -1,6 +1,7 @@
 import {z} from 'zod'
-import { bebidasEschema, CategoriesAPIREsponseSchema, searchFilterSchema } from '../utils/recipes-schema'
+import { bebidasEschema, CategoriesAPIREsponseSchema, drinkSchema, searchFilterSchema } from '../utils/recipes-schema'
 
 export type Categories = z.infer<typeof CategoriesAPIREsponseSchema> // Definimos el type de categorias basandonos en el esquema creado que recibe los datos de la API
 export type SearchFilter = z.infer<typeof searchFilterSchema>
-export type Drikns = z.infer<typeof bebidasEschema>
+export type Drinks = z.infer<typeof bebidasEschema>
+export type Drink = z.infer<typeof drinkSchema> // Tipo para una bebida individual
