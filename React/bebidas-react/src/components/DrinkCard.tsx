fae2e4b0dd0c74bc22108a25695ababf7b4b93e4
Gael Arzubide <arzubide.ga@gmail.com>
2025-10-27 14:51:35 -0600

    se obtiene el id de la bebida conectadolo con el store

--- React/bebidas-react/src/components/DrinkCard.tsx
+++ React/bebidas-react/src/components/DrinkCard.tsx
@@ -1,10 +1,14 @@
 import type { Drink } from "../types"
+import { useAppStore } from "../store/useAppStore"
 
 type DrinkCardProps = {
     drink: Drink
 }
 
 export default function DrinkCard({drink} : DrinkCardProps) {
+    
+    const selectRecipie = useAppStore((state) => state.selectRecipie)
+
     return (
         <div className="border border-gray-300 rounded-2xl p-3 shadow-2xl mb-15 ml-2.5 mr-2.5">
             <div>
@@ -19,6 +23,7 @@ export default function DrinkCard({drink} : DrinkCardProps) {
                 <button
                     type="button"
                     className="bg-orange-400 hover:bg-orange-500 cursor-pointer p-5 rounded-2xl text-white w-full"
+                    onClick={()=> {selectRecipie(drink.idDrink)}} // Le pasamos el id de la bebida
                 >
                     Mas detalles
                 </button>
