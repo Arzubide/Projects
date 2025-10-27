a557443e84c51ae60f20270cc94c925c2fc06234
Gael Arzubide <arzubide.ga@gmail.com>
2025-10-27 15:06:56 -0600

    se crea el esquema para los detalles de la bebida

--- React/bebidas-react/src/utils/recipes-schema.ts
+++ React/bebidas-react/src/utils/recipes-schema.ts
@@ -39,3 +39,28 @@ export const drinkSchema = z.object({
 export const bebidasEschema = z.object({
     drinks : z.array(drinkSchema)
 })
+
+
+export const ingredientsSchema = z.object({
+    idDrink: z.string(),
+    strDrink: z.string(),
+    strDrinkThumb: z.string(),
+    strInstructions: z.string(),
+    strInstructionsES : z.string(),
+    strIngredient1: z.string().nullable(),
+    strIngredient2: z.string().nullable(),
+    strIngredient3: z.string().nullable(),
+    strIngredient4: z.string().nullable(),
+    strIngredient5: z.string().nullable(),
+    strIngredient6: z.string().nullable(),
+    strMeasure1: z.string().nullable(),
+    strMeasure2: z.string().nullable(),
+    strMeasure3: z.string().nullable(),
+    strMeasure4: z.string().nullable(),
+    strMeasure5: z.string().nullable(),
+    strMeasure6: z.string().nullable(),
+})
+
+export const ingredientsList = z.object({
+    drinks : z.array(ingredientsSchema)
+})
