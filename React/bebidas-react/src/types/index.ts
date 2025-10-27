344aae285f6eebc2e21e352123375b02361f6c88
Gael Arzubide <arzubide.ga@gmail.com>
2025-10-27 15:29:52 -0600

    se crea el type para el store de receta

--- React/bebidas-react/src/types/index.ts
+++ React/bebidas-react/src/types/index.ts
@@ -1,7 +1,8 @@
 import {z} from 'zod'
-import { bebidasEschema, CategoriesAPIREsponseSchema, drinkSchema, searchFilterSchema } from '../utils/recipes-schema'
+import { bebidasEschema, CategoriesAPIREsponseSchema, drinkSchema, ingredientsSchema, searchFilterSchema } from '../utils/recipes-schema'
 
 export type Categories = z.infer<typeof CategoriesAPIREsponseSchema> // Definimos el type de categorias basandonos en el esquema creado que recibe los datos de la API
 export type SearchFilter = z.infer<typeof searchFilterSchema>
 export type Drinks = z.infer<typeof bebidasEschema>
 export type Drink = z.infer<typeof drinkSchema> // Tipo para una bebida individual
+export type DetailsDrink = z.infer<typeof ingredientsSchema> // Creamos el type para el state de selectedRecipie en el store
