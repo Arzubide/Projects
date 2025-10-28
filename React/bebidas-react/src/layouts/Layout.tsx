9ae523ee6059084076f432018f124014af44a848
Gael Arzubide <arzubide.ga@gmail.com>
2025-10-28 14:45:18 -0600

    se agrega el modal de manera global a nuestra aplicacion

--- React/bebidas-react/src/layouts/Layout.tsx
+++ React/bebidas-react/src/layouts/Layout.tsx
@@ -1,5 +1,6 @@
 import Header from "../components/Header";
 import { Outlet } from "react-router-dom";
+import Modal from "../components/Modal";
 
 // Layout creado para mostrar componentes que tengan en comun paginas
 export default function Layout() {
@@ -7,9 +8,10 @@ export default function Layout() {
         <>
             <Header/>
             <main className="container mx-auto py-16">
-                
                 <Outlet/> {/*Se agrega Outlet para tambien poder rederizar lo que tenga en las demas paginas aparte del header en este caso */}
             </main>
+
+            <Modal/>
         </>
     )
 }
