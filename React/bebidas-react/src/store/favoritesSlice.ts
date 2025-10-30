5f5c3399d97dd1f05a098219f927fc0945970f50
Gael Arzubide <arzubide.ga@gmail.com>
2025-10-30 15:02:21 -0600

    se setea la bebidas al store de favoritos

--- React/bebidas-react/src/store/favoritesSlice.ts
+++ React/bebidas-react/src/store/favoritesSlice.ts
@@ -3,9 +3,22 @@ import type { DetailsDrink } from "../types"
 
 export type favoriteSliceType = {
     favorites : DetailsDrink[] // Guardaremos la informacion de la receta
-
+    handleClickFavorite : (drink : DetailsDrink) => void
 }
 
-export const createFavoriteSlice : StateCreator<favoriteSliceType> = () => ({
-    favorites : []
+
+
+export const createFavoriteSlice : StateCreator<favoriteSliceType> = (set,get) => ({
+    // Metodo get nos sirve para obtener los valores dentro de nuestro store
+    favorites : [],
+    handleClickFavorite : (recipie)=> {
+        if (get().favorites.some(favorite => favorite.idDrink === recipie.idDrink)) {
+            //Comprobamos que el elemento que se añada a favoritos no exista ya en favoritos
+            console.log('ya existe el elemento')
+        }else{
+            set({
+                favorites : [...get().favorites, recipie]
+            })
+        }
+    }  
 })
