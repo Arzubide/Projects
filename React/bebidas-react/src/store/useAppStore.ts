c0340d75b3acbe7505a8876a2ee60ad3a9f6dae0
Gael Arzubide <arzubide.ga@gmail.com>
2025-10-30 14:46:49 -0600

    se conecta favorites con el store de la aplicacion

--- React/bebidas-react/src/store/useAppStore.ts
+++ React/bebidas-react/src/store/useAppStore.ts
@@ -1,7 +1,8 @@
 import { create } from 'zustand'
 import { createRecipeSlice, type RecipesSliceType } from './recipeSlice'
+import { type favoriteSliceType, createFavoriteSlice} from "./favoritesSlice"
 
-export const useAppStore = create<RecipesSliceType>((...a)=>({
+export const useAppStore = create<RecipesSliceType & favoriteSliceType>((...a)=>({
     /* 
         ...a es el operador rest que captura todos los argumentos que create le pasa a la funcion en un array
             en este caso internamente le pasamos los 3 argumentos set(funcion para actualizar estado) ,get(funcion para leer el estado actual) y api(objeto con metodos internos de zustand)
@@ -10,5 +11,6 @@ export const useAppStore = create<RecipesSliceType>((...a)=>({
         create((...a) => ({...}))
         create((set, get, api) => ({...}))
     */
-    ...createRecipeSlice(...a)
+    ...createRecipeSlice(...a),
+    ...createFavoriteSlice(...a)
 }))
