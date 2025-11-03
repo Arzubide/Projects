f2d13ec9aeda9980f78c71683026cb591ea9b0f6
Gael Arzubide <arzubide.ga@gmail.com>
2025-11-03 15:34:10 -0600

    se renderiza el contenido de favoritos en su pestaña

--- React/bebidas-react/src/pages/FavoritesPages.tsx
+++ React/bebidas-react/src/pages/FavoritesPages.tsx
@@ -1,6 +1,29 @@
+import DrinkCard from "../components/DrinkCard"
+import { useAppStore } from "../store/useAppStore"
+import { useMemo } from "react"
 
 export default function FavoritesPages() {
-  return (
-    <div>FavoritesPages</div>
-  )
+  
+    const favorites = useAppStore((state)=>state.favorites) // Traemos las bebidas favoritas
+    const hasFavorites = useMemo(()=> favorites.length>0, [favorites]) // Si existen bebidas en favoritos
+
+    return (
+        <>
+          <h1 className="font-bold uppercase text-2xl text-center">Favorites</h1>
+          {hasFavorites ? 
+          
+            <div className="grid grid-cols-2 pt-5  md:grid-cols-3 2xl:grid-cols-4">
+              {favorites.map((favoriteDrink)=>(
+                <DrinkCard
+                  key={favoriteDrink.idDrink}
+                  drink = {favoriteDrink}
+                />
+              ))}
+            </div>
+        
+          :
+              <p className="text-center mt-5 mb-5 text-gray-500">Aun no tienes bebidas favoritas</p>
+          }
+        </>
+    )
 }
