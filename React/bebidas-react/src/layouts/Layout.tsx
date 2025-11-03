f9157ab2dea5e54fdca24288bd1dbfa5c598e283
Gael Arzubide <arzubide.ga@gmail.com>
2025-11-03 15:10:33 -0600

    agregamos local storage a la pagina

--- React/bebidas-react/src/layouts/Layout.tsx
+++ React/bebidas-react/src/layouts/Layout.tsx
@@ -1,9 +1,15 @@
 import Header from "../components/Header";
 import { Outlet } from "react-router-dom";
 import Modal from "../components/Modal";
+import { useEffect } from "react";
+import { useAppStore } from "../store/useAppStore";
 
 // Layout creado para mostrar componentes que tengan en comun paginas
 export default function Layout() {
+
+    const loadFromStoragee = useAppStore((state)=>state.loadFromStorage) // Traemos los datos del localstorage
+    useEffect(()=>{loadFromStoragee()},[]) //Cada vez qu es actiulice se estara cambiando
+
     return (
         <>
             <Header/>
