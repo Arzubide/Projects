71b1aa5e078da95b3ac27412672f2cda3f358ce8
Gael Arzubide <arzubide.ga@gmail.com>
2025-11-13 18:41:14 -0600

    s

--- React/FullStack/rest_api_node_ts_server/src/servidor.ts
+++ React/FullStack/rest_api_node_ts_server/src/servidor.ts
@@ -5,8 +5,8 @@ const servidor = express() // Creamos el servidor
 // Dentro del servidor existen distintas peticiones, get, post, delete, put, patch (router.ts)
 
 //Routing - Conectamos el router a un prefijo de URL
-servidor.use('/', router) // Use es una función que se ejecuta entre que llega una petición HTTP y se envía la respuesta.
-
+servidor.use('/api/products', router) // Use es una función que se ejecuta entre que llega una petición HTTP y se envía la respuesta.
+// al momento de nosotros definir esa direccion, le definimos todas las direcciones de lo que tenga router
 
 
 export default servidor 
