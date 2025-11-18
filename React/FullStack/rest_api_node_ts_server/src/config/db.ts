f2debc9eef286a8cfd6e3ec8a199475ea0b6f3df
Gael Arzubide <arzubide.ga@gmail.com>
2025-11-18 15:20:53 -0600

    se configura para que se agreguen todos lo modelos creados

--- React/FullStack/rest_api_node_ts_server/src/config/db.ts
+++ React/FullStack/rest_api_node_ts_server/src/config/db.ts
@@ -1,11 +1,13 @@
 import dotenv from 'dotenv' // Instalamos esta dependencia para ocultar la url de la base de datos 
-import {Sequelize} from "sequelize"
-// const { Sequelize } = require('sequelize');
+import {Sequelize} from "sequelize-typescript"
+import Product from '../Models/Product.model'
+
 dotenv.config() // accedemos a todas las configuraciones
 
 
 export const dataBase = new Sequelize(process.env.DATA_BASE_EXTERNAL!, {
-  dialect: 'postgres' // expresamos que bd es 
+  dialect: 'postgres', // expresamos que bd es 
+  models : [__dirname + '/../models/**/*.ts'] // Todos los archivos ts en models se consideraran modelos
 })// accedemos a la variable que creamos en env 
  // process.env.DATA_BASE_EXTERNAL!
 
