0c6fb84ab9139390843908a7d0a79fd095200bef
Gael Arzubide <arzubide.ga@gmail.com>
2025-11-24 14:50:11 -0600

    se guardan los datos recibidos del usuario a la BD sin validaciones

--- React/FullStack/rest_api_node_ts_server/src/Handlers/products.ts
+++ React/FullStack/rest_api_node_ts_server/src/Handlers/products.ts
@@ -1,5 +1,15 @@
 import {Request,Response} from "express"
+import Product from "../Models/Product.model";
 
 export const createProduct = async (req : Request , res:Response) => {
-    res.json("Create new product")
+    // console.log(req.body) Vemos los datos que se obtienen
+
+    // 1era Forma de almacenar productos en la BD
+    // const product = new Product(req.body)  Creamos una instancia del modelo product con los datos que llegan al momento de hacer esa peticion "POST"
+    // await product.save()
+
+    //2da forma de almacenar productos en la BD
+    const product = await Product.create(req.body)
+    res.json(product)
+
 }
