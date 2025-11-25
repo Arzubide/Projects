1f8740d48e839cfc5519ffc0651db828ecc044fb
Gael Arzubide <arzubide.ga@gmail.com>
2025-11-25 17:07:01 -0600

    se agrega el middleware para get

--- React/FullStack/rest_api_node_ts_server/src/router.ts
+++ React/FullStack/rest_api_node_ts_server/src/router.ts
@@ -1,16 +1,13 @@
 import {Router} from "express"
-import {createProduct} from "./Handlers/products"; // Importamos la funcion del metodo POST
+import {createProduct, getProduct} from "./Handlers/products"; // Importamos la funcion del metodo POST
 import {handleInputError, validation} from "./middleware";
 
 const router = Router() // Accedemos a todas las funciones del router de express
 
 
-router.get('/', (req, res)=>{ // Siempre se pasan los parametros request (lo que enviamos a la pagina) (req) y response (respuestas de la pagina) (res)
-    // res tiene metodos como 
-    // send: envia datos a la pantalla
-    // json: eviamos datos json
-    res.send("Hola mundo desde GET")
-}) 
+router.get('/',
+    getProduct,
+    )
 
 router.post('/',
     validation, // middleware para las validaciones
