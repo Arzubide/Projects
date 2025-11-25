660233a2e72926b9da605c140b8296d255e3aea8
Gael Arzubide <arzubide.ga@gmail.com>
2025-11-25 15:41:19 -0600

    se crea los middlewares para la operacion post

---
+++ React/FullStack/rest_api_node_ts_server/src/middleware/index.ts
@@ -0,0 +1,36 @@
+import express from "express";
+import {body, validationResult} from 'express-validator'
+import {Default} from "sequelize-typescript";
+
+export const validation = [
+    // creamos un array de validaciones para usarse desde el router
+    body("name")
+        .notEmpty()
+        .isString()
+        .withMessage("Name is required"), // Las validaciones deben retornarse como array
+    
+    body("price")
+        .notEmpty()
+        .withMessage("Price is required")
+        .isNumeric()
+        .withMessage("Price must be a number")
+        .custom((value: number) => {
+            if (value <= 0) {
+                throw new Error('Price must be greater than 0')
+            }
+            return true // Importante: retornar true cuando la validación pasa
+        }),
+    body("availability")
+        .optional() // Hace el campo opcional para que no sea requerido
+        .isBoolean()
+        .withMessage("Availability must be boolean")
+]
+
+export const handleInputError = (req: express.Request, res: express.Response, next) => {
+
+    let errors = validationResult(req) // Vemos los errores que existen
+    if (!errors.isEmpty()) {
+        return res.status(400).send({errors: errors.array()}) // Cancelamos el POST
+    }
+    next() // Continua con el siguiente middleware
+}
