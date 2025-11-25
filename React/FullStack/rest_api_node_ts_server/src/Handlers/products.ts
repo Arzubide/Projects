0d62c50eff03da0d7f38464164bb9fa2b96cd31c
Gael Arzubide <arzubide.ga@gmail.com>
2025-11-25 15:38:51 -0600

    se limpia product

--- React/FullStack/rest_api_node_ts_server/src/Handlers/products.ts
+++ React/FullStack/rest_api_node_ts_server/src/Handlers/products.ts
@@ -1,5 +1,6 @@
 import {Request,Response} from "express"
 import Product from "../Models/Product.model";
+import {check, validationResult} from "express-validator" // Librerias para las validaciones
 
 export const createProduct = async (req : Request , res:Response) => {
     // console.log(req.body) Vemos los datos que se obtienen
@@ -7,6 +8,24 @@ export const createProduct = async (req : Request , res:Response) => {
     // 1era Forma de almacenar productos en la BD
     // const product = new Product(req.body)  Creamos una instancia del modelo product con los datos que llegan al momento de hacer esa peticion "POST"
     // await product.save()
+
+    // Validaciones desde la creacion del producto de hacen de la siguiente manera
+    // await check("name", "Name is required")
+    //     .notEmpty() // Validamos que no este vacio ese campo
+    //     .run(req) // Obtenemos el valor del request
+    // await check("price", "Price is required")
+    //     .notEmpty()
+    //     .isNumeric() // Validamos que sea numero
+    //     .custom((value:number) => {
+    //         value > 0 // value es el valor que llega de request
+    //     }).withMessage("precio no valido") // Validacion personalizada
+    //     .run(req) //
+
+    //Se crea una funcion aparte para las validaciones de los errores
+    // let errors = validationResult(req) // Vemos los errores que existen
+    // if (!errors.isEmpty()) {
+    //     return res.status(400).send({errors: errors.array()}) // Cancelamos el POST
+    // }
 
     //2da forma de almacenar productos en la BD
     const product = await Product.create(req.body)
