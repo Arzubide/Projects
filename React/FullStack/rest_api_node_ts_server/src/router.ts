1f614a3c800c584361731bc6a20efcc0681669a9
Gael Arzubide <arzubide.ga@gmail.com>
2025-11-25 15:45:41 -0600

    se termina de agregar los middlewares para el endpoint post

--- React/FullStack/rest_api_node_ts_server/src/router.ts
+++ React/FullStack/rest_api_node_ts_server/src/router.ts
@@ -1,5 +1,6 @@
 import {Router} from "express"
 import {createProduct} from "./Handlers/products"; // Importamos la funcion del metodo POST
+import {handleInputError, validation} from "./middleware";
 
 const router = Router() // Accedemos a todas las funciones del router de express
 
@@ -11,7 +12,10 @@ router.get('/', (req, res)=>{ // Siempre se pasan los parametros request (lo que
     res.send("Hola mundo desde GET")
 }) 
 
-router.post('/', createProduct ) // createProduct va a ser de metodo POST
+router.post('/',
+    validation, // middleware para las validaciones
+    handleInputError, // middle para mostrar los errores
+    createProduct ) // createProduct va a ser de metodo POST
 
 router.put('/', (req,res)=>{
     console.log("Desde PUT ")  
