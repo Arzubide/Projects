181a9eec91d0c096dbf19c0b31df0d44e5e9c31f
Gael Arzubide <arzubide.ga@gmail.com>
2025-11-26 18:56:20 -0600

    se agrega el middleware al endpoint de pathc

--- React/FullStack/rest_api_node_ts_server/src/router.ts
+++ React/FullStack/rest_api_node_ts_server/src/router.ts
@@ -1,5 +1,5 @@
 import {Router} from "express"
-import {createProduct, getProduct, getProductById, updateProduct} from "./Handlers/products"; // Importamos la funcion del metodo POST
+import {createProduct, getProduct, getProductById, updateAvailableProduct, updateProduct} from "./Handlers/products"; // Importamos la funcion del metodo POST
 import {handleInputError, validation, validationById} from "./middleware";
 
 const router = Router() // Accedemos a todas las funciones del router de express
@@ -12,23 +12,24 @@ router.get('/',
 router.get('/:id', // Routing dinamico, a traves de la URL se le pasa el campo id
     validationById,
     handleInputError,
-    getProductById,
+    getProductById
 )
 
 router.post('/',
     validation, // middleware para las validaciones
     handleInputError, // middle para mostrar los errores
-    createProduct ) // createProduct va a ser de metodo POST
+    createProduct
+) // createProduct va a ser de metodo POST
 
 router.put('/:id',
     validation, // Validaciones para la acutualizacion del producto
     handleInputError,
-    updateProduct,
-    )
+    updateProduct
+)
 
-router.patch('/', (req,res)=>{
-    console.log("Desde PATCH ")  
-})
+router.patch('/:id',
+    updateAvailableProduct
+)
 
 // La diferencia entre PUT Y PATCH, PUT: remplaza/actualizar completamente un recurso, PATCH modificar unicamente el valor que indicas
 
